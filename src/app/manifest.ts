ef8afcdeded1cf3b5eb4b6758921f19243846ad8
Whokeas <rexleogain@gmail.com>
2026-07-23 15:55:34 +0300

    Add WHOKEAS search favicon and brand icon

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+﻿import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "WHOKEAS ALL IN",
+    short_name: "WHOKEAS",
+    description:
+      "A global marketplace for technology, home, fashion, study, beauty and lifestyle products.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#181511",
+    theme_color: "#181511",
+    icons: [
+      {
+        src: "/brand/search-logo.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
